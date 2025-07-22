aef5f3e6ebd936f70738926e3de9bb2a17460ea8
seanscholte <sean@furison.tech>
2025-07-22 22:15:59 +0200

    fixed column array key mismatch bug

--- resources/views/blocks/columns.blade.php
+++ resources/views/blocks/columns.blade.php
@@ -1,6 +1,10 @@
 <div style="display: grid; gap: 10px; padding: 10px;
             grid-template-columns: repeat({{count($data['cols'])}}, 1fr)" >
     @foreach($data['cols'] as $col)
-        @include('blocks.' . $col['type'], ['data' => $col])
+        <div>
+            @foreach ($col['blocks'] as $block)
+                @include('laraveditorjs::blocks.' . $block['type'], ['data' => $block['data']])
+            @endforeach
+        </div>
     @endforeach
 </div>
